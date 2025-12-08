683bfe16b975a4e812e8d7325ffbec158625780b
Sven Wappler <wappler@wappler.systems>
2025-12-08 23:45:44 +0100

    updates

--- Classes/Controller/AuthorizationController.php
+++ Classes/Controller/AuthorizationController.php
@@ -3,7 +3,10 @@
 namespace WapplerSystems\OidcAddons\Controller;
 
 use Psr\Http\Message\ResponseInterface;
+use TYPO3\CMS\Backend\Attribute\AsController;
+use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
 use TYPO3\CMS\Core\Context\Context;
+use TYPO3\CMS\Core\Http\RedirectResponse as RedirectResponseAlias;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
@@ -14,10 +17,15 @@ class AuthorizationController extends ActionController
     public function redirectToLogoutAction(): ResponseInterface
     {
 
-        debug('dede');
-        exit();
+        $extensionConfiguration = GeneralUtility::makeInstance(ExtensionConfiguration::class);
+        $oidcConfig = $extensionConfiguration->get('oidc');
+        $oidcEndpointLogout = $oidcConfig['oidcEndpointLogout'] ?? null;
 
-        return $this->htmlResponse();
+        $oidcEndpointLogout .= '?redirect_uri=' . urlencode(
+            GeneralUtility::getIndpEnv('TYPO3_SITE_URL')
+        );
+
+        return new RedirectResponseAlias($oidcEndpointLogout);
     }
 
 
--- Classes/Controller/AuthorizationStatusController.php
+++ Classes/Controller/AuthorizationStatusController.php
@@ -3,6 +3,7 @@
 namespace WapplerSystems\OidcAddons\Controller;
 
 use Psr\Http\Message\ResponseInterface;
+use TYPO3\CMS\Backend\Attribute\AsController;
 use TYPO3\CMS\Core\Context\Context;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
@@ -19,6 +20,13 @@ class AuthorizationStatusController extends ActionController
         $isLoggedIn = $context->getPropertyFromAspect('frontend.user', 'isLoggedIn');
 
         $this->view->assign('isLoggedIn', $isLoggedIn);
+
+        if ($this->request->getQueryParams()['loginSuccess'] ?? false) {
+            $this->view->assign('showLoginSuccessMessage', true);
+        }
+        if (($this->request->getQueryParams()['logoutSuccess'] ?? false) || ($this->request->getQueryParams()['logintype'] ?? '' === 'logout')) {
+            $this->view->assign('showLogoutSuccessMessage', true);
+        }
 
         return $this->htmlResponse();
     }
